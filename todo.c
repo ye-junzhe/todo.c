018c9953cd92cc64fa16b477df6eb20aa839b717
Ye Junzhe <junzheyez@gmail.com>
2023-09-30 16:30:27 +0800

    Read user input by line
    (1) fflush() => clear stdin

--- todo.c
+++ todo.c
@@ -1,6 +1,9 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define RELEASE
 
 typedef enum {
     PENDING,
@@ -16,27 +19,28 @@ int todo_count = 0;
 Todo *todos = NULL;
 
 void createTodo() {
+    char *line = NULL;
+    size_t linecap = 0;
+
+    printf("Creating todo: => ");
+
+    // fseek(stdin, 0, SEEK_END);
+    fflush(stdin);
+    getline(&line, &linecap, stdin);
 
     Todo todo;
-
-    char buffer[80];
-    printf("Creating todo\nEnter todo info: ");
-    scanf("%s", buffer); // TODO: read line, not just one word
-
-    todo.todo_info = (char *)malloc(strlen(buffer) + 1); // TODO: malloc according to the input len
-    strncpy(todo.todo_info, buffer, sizeof(buffer));
+    todo.todo_info = (char *)malloc(strlen(line) + 1);
+    strcpy(todo.todo_info, line);
 
     todo.status = PENDING;
 
-    todos = (Todo *)realloc(todos, (todo_count+1)*sizeof(Todo));
+    todos = (Todo *)realloc(todos, (todo_count + 1) * sizeof(Todo));
     todos[todo_count] = todo;
+    todo_count++;
 
-    todo_count ++;
-
-    // printf("TODO Status: %d,\nTODO Info: %s,\nThis is the NO.%d todo\n", todo.status, todo.todo_info, todo_count);
+    printf("New todo added: %s", line);
 }
 
-// TODO: Clear screen
 void printTodos() {
     for (int i = 0; i < todo_count; i++) {
         printf("(%d). %s\n", i + 1, todos[i].todo_info);
@@ -44,45 +48,77 @@ void printTodos() {
 }
 
 void markDoneTodo() {
+    if (todo_count == 0) {
+        printf("No Todos yet\n");
+        return;
+    }
+
     int which_one;
-    printf("Which todo you wish to remove ?\n");
+    printf("Which todo you wish to remove ? => ");
     scanf("%d", &which_one);
+
+    if (!isdigit(which_one)) {
+        printf("Not a digit");
+        return;
+    }
+
+    if (which_one > todo_count || which_one <= 0) {
+        printf("No Todo has that index\n");
+        return;
+    } 
+
     for (int i = which_one - 1; i <= todo_count - 1; i++) {
         todos[i] = todos[i + 1];
     }
     todo_count --;
 }
 
+void cleanMem() {
+    free(todos);
+}
+
 void listenUserInput() {
+    system("clear");
     char userInput;
     while (1) {
-        printf("c => create\ns => show\nd => mark done\nq => quit\n: ");
+        printf("c => create\ns => show\nd => mark done\nq => quit\n");
         printf(
-                 "                      d8b                                               d8b         \n"
-                 "   d8P                88P                        d8P                    88P         \n"
-                 "d888888P             d88                      d888888P                 d88          \n"
-                 "  ?88'   d8888b  d888888   d8888b  .d888b,      ?88'   d8888b      d888888   d8888b \n"
-                 "  88P   d8P' ?88d8P' ?88  d8P' ?88 ?8b,         88P   d8P' ?88    d8P' ?88  d8P' ?88\n"
-                 "  88b   88b  d8888b  ,88b 88b  d88   `?8b       88b   88b  d88    88b  ,88b 88b  d88\n"
-                 "  `?8b  `?8888P'`?88P'`88b`?8888P'`?888P'       `?8b  `?8888P'    `?88P'`88b`?8888P \n"
-                 "                                                                                    \n"
-                 "                                                                                    \n"
-                 "                                                                                    \n"
+            "                      d8b                                               d8b         \n"
+            "   d8P                88P                        d8P                    88P         \n"
+            "d888888P             d88                      d888888P                 d88          \n"
+            "  ?88'   d8888b  d888888   d8888b  .d888b,      ?88'   d8888b      d888888   d8888b \n"
+            "  88P   d8P' ?88d8P' ?88  d8P' ?88 ?8b,         88P   d8P' ?88    d8P' ?88  d8P' ?88\n"
+            "  88b   88b  d8888b  ,88b 88b  d88   `?8b       88b   88b  d88    88b  ,88b 88b  d88\n"
+            "  `?8b  `?8888P'`?88P'`88b`?8888P'`?888P'       `?8b  `?8888P'    `?88P'`88b`?8888P \n"
+            "                                                                                    \n"
+            "                                                                                    \n"
+            "                                                                                    \n"
         );
         printTodos();
-        scanf(" %c", &userInput);
+        fflush(stdin);
+        scanf("%c", &userInput);
         switch (userInput) {
             case 'c':
                 createTodo();
+                #ifdef RELEASE
+                system("clear");
+                #endif /* ifdef  RELEASE */
                 break;
             case 's':
                 printTodos();
+                #ifdef RELEASE
+                system("clear");
+                #endif /* ifdef  RELEASE */
                 break;
             case 'd':
                 markDoneTodo();
+                #ifdef RELEASE
+                system("clear");
+                #endif /* ifdef  RELEASE */
                 break;
             case 'q':
                 printf("Quitting\n");
+                cleanMem();
                 return;
             default:
                 printf("Not an available option\n");
