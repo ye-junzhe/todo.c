3d6134d2c5f75e2023e474c2d92b21478ada5c64
Ye Junzhe <junzheyez@gmail.com>
2023-10-04 21:28:07 +0800

    (1) Add arrows (2) Disable echo when typing

--- todo.c
+++ todo.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <termios.h>
+#include <unistd.h>
 
 #define RELEASE
 
@@ -10,8 +12,6 @@
 #else
 #define CLEAR
 #endif
-
-#define ASCII_OFFSET_DIGIT 48
 
 typedef enum { PENDING, COMPLETE } Status;
 
@@ -22,16 +22,32 @@ typedef struct {
 
 typedef struct {
     char *log_info;
-    int isCreated;
-    int isDone;
-    int isNotAvailableOp;
-    int isRenamed;
+    int isCreated,
+        isDone,
+        isNotAvailableOp,
+        isRenamed;
 } Logger;
 
 int todo_count = 0;
+int previous_cursor = 0;
+int next_cursor = 0;
 Todo *todos = NULL;
 
 Logger logger = {"Do Something!\n", 0, 0, 0, 0};
+
+struct termios oldt, newt;
+
+void disableEcho() {
+    // Disable line buffering and echo
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+}
+
+void enableEcho() {
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+}
 
 void load_log_info(char *info) {
     logger.log_info = info;
@@ -45,7 +61,7 @@ void log_info() {
         printf("%s", logger.log_info);
 }
 
-void printTodos() {
+void displayTodos() {
     for (int i = 0; i < todo_count; i++) {
         printf("(%d). %s\n", i + 1, todos[i].todo_info);
     }
@@ -65,12 +81,48 @@ void printDashBoard() {
         "                                                                                    \n"
     );
 
-    printTodos();
+    refreshUI();
     log_info();
     logger.isDone = 0;
     logger.isCreated = 0;
     logger.isNotAvailableOp = 0;
     logger.isRenamed = 0;
+}
+
+void concatArrow(int target) {
+
+    for(int i = 0;; i++) {
+        if(todos[target].todo_info[i] == '\n') {
+            todos[target].todo_info[i] = ' ';
+            break;
+        }
+    }
+
+    previous_cursor = next_cursor;
+    next_cursor = target; // cursor here is the index of the new todo
+
+    todos[next_cursor].todo_info = strcat(todos[next_cursor].todo_info, "<-\n");
+
+    if (todo_count == 0 || previous_cursor > todo_count - 1 || next_cursor == previous_cursor) return;
+
+    int length = strlen(todos[previous_cursor].todo_info);
+    char modifiedStr[length - 2];
+    strncpy(modifiedStr, todos[previous_cursor].todo_info, length - 2);
+    modifiedStr[length - 4] = '\n';
+    modifiedStr[length - 3] = '\0';
+    strcpy(todos[previous_cursor].todo_info, modifiedStr);
+}
+
+void arrowUp() {
+    if (next_cursor - 1 < 0) return;
+
+    concatArrow(next_cursor - 1);
+}
+
+void arrowDown() {
+    if (next_cursor + 1 >= todo_count) return;
+
+    concatArrow(next_cursor + 1);
 }
 
 void createTodo() {
@@ -81,16 +133,20 @@ void createTodo() {
 
     // fseek(stdin, 0, SEEK_END);
     fflush(stdin);
+
+    enableEcho();
     getline(&line, &linecap, stdin);
+    disableEcho();
 
     Todo todo;
-    todo.todo_info = (char *)malloc(strlen(line) + 1);
+    todo.todo_info = (char *)malloc(strlen(line) + 1 + 3);
     strcpy(todo.todo_info, line);
 
     todo.status = PENDING;
 
     todos = (Todo *)realloc(todos, (todo_count + 1) * sizeof(Todo));
     todos[todo_count] = todo;
+    concatArrow(todo_count);
     todo_count++;
 
     load_log_info("New todo created\n");
@@ -103,26 +159,27 @@ void renameTodo() {
         return;
     }
 
-    printf("Which todo you wish to rename? => ");
-
-    fflush(stdin);
-    int which_one = fgetc(stdin) - ASCII_OFFSET_DIGIT;
-
-    if (which_one > todo_count || which_one <= 0) {
-        load_log_info("No Todo has that index\n");
-        return;
-    }
-
     char *line = NULL;
     size_t linecap = 0;
 
     printf("Renaming todo: => ");
 
     fflush(stdin);
+    enableEcho();
     getline(&line, &linecap, stdin);
+    disableEcho();
 
-    todos[which_one - 1].todo_info = (char *)realloc(todos[which_one - 1].todo_info, strlen(line));
-    strcpy(todos[which_one - 1].todo_info, line);
+    todos[next_cursor].todo_info = (char *)realloc(todos[next_cursor].todo_info, strlen(line));
+    strcpy(todos[next_cursor].todo_info, line);
+
+    for(int i = 0;; i++) {
+        if(todos[next_cursor].todo_info[i] == '\n') {
+            todos[next_cursor].todo_info[i] = ' ';
+            break;
+        }
+    }
+    todos[next_cursor].todo_info = strcat(todos[next_cursor].todo_info, "<-\n");
+
     load_log_info("Todo Renamed\n");
     logger.isRenamed = 1;
 }
@@ -133,26 +190,29 @@ void markDoneTodo() {
         return;
     }
 
-    printf("Which todo you wish to remove ? => ");
-    fflush(stdin);
-    int which_one = fgetc(stdin) - ASCII_OFFSET_DIGIT;
+    // TODO: Show todos that are already down, just declare a new Todo struct, and put this COMPLETE todo into it
+    // It acts as exatly the same as the original todo!
+    // It will log every time
+    // And maybe we can move the cursor right and left
+    todos[next_cursor].status = COMPLETE;
 
-    if (which_one > todo_count || which_one <= 0) {
-        load_log_info("No Todo has that index\n");
-        return;
-    }
-
-    for (int i = which_one - 1; i <= todo_count - 1; i++) {
+    for (int i = next_cursor; i <= todo_count - 1; i++) {
         todos[i] = todos[i + 1];
     }
-    todo_count --;
+    todo_count--;
+    printf("%d\n", todo_count);
+
+    if (todo_count == 0) return;
+    if (next_cursor == todo_count) // cursor at the highest todo
+        concatArrow(next_cursor - 1);
+    else concatArrow(next_cursor);
 
     load_log_info("Todo done!\n");
     logger.isDone = 1;
 }
 
 void printHelp() {
-    printf("c => create\nr => rename\ns => show\nd => mark done\nq => quit\n");
+    printf("c => create\nk => up\nj => down\nr => rename\ns => refresh\nx => mark done\nq => quit\n");
 }
 
 void cleanMem() {
@@ -160,6 +220,8 @@ void cleanMem() {
 }
 
 void listenUserInput() {
+
+    disableEcho();
 
     CLEAR;
 
@@ -178,14 +240,14 @@ void listenUserInput() {
                 CLEAR;
                 break;
             case 's':
-                printTodos();
+                displayTodos();
                 CLEAR;
                 break;
             case 'r':
                 renameTodo();
                 CLEAR;
                 break;
-            case 'd':
+            case 'x':
                 markDoneTodo();
                 CLEAR;
                 break;
@@ -193,12 +255,21 @@ void listenUserInput() {
                 CLEAR;
                 printHelp();
                 break;
+            case 'k':
+                CLEAR;
+                arrowUp();
+                break;
+            case 'j':
+                CLEAR;
+                arrowDown();
+                break;
             case 'q':
                 printf("Quitting\n");
                 cleanMem();
                 return;
             default:
                 logger.isNotAvailableOp = 1;
+                printf("%c", userInput);
                 load_log_info("Not an available option\n");
                 CLEAR;
         }
